e7a808ed78b6d96eeb84ddb0b60a27479f69e88f
Kinzang Wangdi <wangdikinzang544@gmail.com>
2025-07-23 17:51:49 +0600

    Connected to Google ai api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
 import React, { useState } from 'react'
 import styles from './App.module.css'
+import { GoogleGenerativeAI } from "@google/generative-ai";
 import { Chat } from './components/chat/chat'
 import { Controls } from './components/controls/controls'
+
+const googleai = new GoogleGenerativeAI(import.meta.env.VITE_GOGGLE_AI_API_KEY);
+const gemini = googleai.getGenerativeModel({ model: "gemini-1.5-flash" });
+const chat = gemini.startChat({ history: [] });
+
 
 function App() {
 
   const [Messages, setMessages] = useState([])
 
-  function HandleContentSend(content) {
-    setMessages(prevMessages => [
-      ...prevMessages,
-      { role: 'user', content }
-    ]); 
+  function addMessage(message) {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }
+
+  async function HandleContentSend(content) {
+    addMessage({ content, role: "user" });
+    try {
+      const result = await chat.sendMessage(content);
+      addMessage({ content: result.response.text(), role: "assistant" });
+    } catch (error) {
+      addMessage({
+        content: "Sorry, I couldn't process your request. Please try again!",
+        role: "system",
+      });
+    }
   }
   
 
